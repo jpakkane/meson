9f097013397118bc76d5c66305a1dd4a399505ba
Dylan Baker <dylan@pnwbakers.com>
2023-03-08 12:17:36 -0800

    make gui_app an alias of win_subsystem in the interpreter
    There's no reason to plumb both gui_app and win_subsystem through, we
    can just set win_subsystem to an appropriate value, and drop gui_app
    altogether

--- mesonbuild/backend/vs2010backend.py
+++ mesonbuild/backend/vs2010backend.py
@@ -916,13 +916,9 @@ class Vs2010Backend(backends.Backend):
         self.handled_target_deps[target.get_id()] = []
         if isinstance(target, build.Executable):
             conftype = 'Application'
-            if target.gui_app is not None:
-                if not target.gui_app:
-                    subsystem = 'Console'
-            else:
-                # If someone knows how to set the version properly,
-                # please send a patch.
-                subsystem = target.win_subsystem.split(',')[0]
+            # If someone knows how to set the version properly,
+            # please send a patch.
+            subsystem = target.win_subsystem.split(',')[0]
         elif isinstance(target, build.StaticLibrary):
             conftype = 'StaticLibrary'
         elif isinstance(target, build.SharedLibrary):
